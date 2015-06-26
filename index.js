cc8f2408864d84edd6071534933a8f3a2cfa55a8
teawithfruit <teawithfruit@gmail.com>
2015-06-26 19:40:15 +0200

    fixed mime checking

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 var Teatime = undefined;
 
 var Q = require('q');
+var r = require('request');
 var request = require('request-promise');
 var cheerio = require('cheerio');
 var fs = require('fs-extra');
 var mime = require('mime-types');
+var fileType = require('file-type');
 var util = require('util');
 var url = require('url');
 var EventEmitter = require('events').EventEmitter;
@@ -45,53 +47,68 @@ Teatime.prototype.open = function(theUrl) {
 
   visited.push(urlParsed.href);
 
-  if(urlParsed.href && /http|https/.test(urlParsed.protocol) && !/application|image|video/.test(mime.lookup(urlParsed.href)) ) {
-    request({ uri: urlParsed.href, simple: false, resolveWithFullResponse: true })
-    .then(function(response) {
+  if(urlParsed.href && /http|https/.test(urlParsed.protocol)) {
+    r.get(urlParsed.href).once('data', function(chunk) {
+      var theFileType = undefined;
 
-      if(response.request._redirect.redirects.length <= 0) {
-        var testDomain = new RegExp(that.options.domain, 'g');
-        if(testDomain.test(response.request.uri.href.match(matchHostname)[0])) {
-          if(/text\/html|text\/xml/.test(response.headers['content-type'])) {
+      theFileType = fileType(chunk);
+      if(theFileType) theFileType = theFileType.mime;
+      this.abort();
 
-            var $ = cheerio.load(response.body);
-            var newUrls = $('a');
+      if(!/application|image|video/.test(theFileType) ) {
+        request({ uri: urlParsed.href, simple: false, resolveWithFullResponse: true })
+        .then(function(response) {
 
-            if(that.options.crawl == true && newUrls != null) {
-              
-              $(newUrls).each(function(i, link) {
-                if($(link).attr('href') != undefined) {
-                  var theNew = url.parse($(link).attr('href')).href;
+          if(response.request._redirect.redirects.length <= 0) {
+            var testDomain = new RegExp(that.options.domain, 'g');
+            if(testDomain.test(response.request.uri.href.match(matchHostname)[0])) {
+              if(/text\/html|text\/xml/.test(response.headers['content-type'])) {
 
-                  if($('base').attr('href') && !/^\/$/i.test(theNew) && !/^http.*$/i.test(theNew)) theNew = $('base').attr('href') + theNew;
-                  if(theNew && url.parse(theNew).host == null) theNew = helpers.absoluteUri(response.request.uri.href, theNew);
+                var $ = cheerio.load(response.body);
+                var newUrls = $('a');
 
-                  if(theNew) {
-                    theNew = theNew.replace(/\?.*$/, '');
-                    theNew = theNew.replace(/\#.*$/, '');
-                    theNew = theNew.replace(/\;.*$/, '');
-                  }
+                if(that.options.crawl == true && newUrls != null) {
+                  
+                  $(newUrls).each(function(i, link) {
+                    if($(link).attr('href') != undefined) {
+                      var theNew = url.parse($(link).attr('href')).href;
 
-                  if(pending.indexOf(theNew) == -1 && visited.indexOf(theNew) == -1 && !/mailto:|javascript/i.test(theNew) && theNew != undefined && theNew != null) pending.push(theNew);
+                      if($('base').attr('href') && !/^\/$/i.test(theNew) && !/^http.*$/i.test(theNew)) theNew = $('base').attr('href') + theNew;
+                      if(theNew && url.parse(theNew).host == null) theNew = helpers.absoluteUri(response.request.uri.href, theNew);
+
+                      if(theNew) {
+                        theNew = theNew.replace(/\?.*$/, '');
+                        theNew = theNew.replace(/\#.*$/, '');
+                        theNew = theNew.replace(/\;.*$/, '');
+                      }
+
+                      if(pending.indexOf(theNew) == -1 && visited.indexOf(theNew) == -1 && !/mailto:|javascript/i.test(theNew) && theNew != undefined && theNew != null) pending.push(theNew);
+                    }
+                  });
                 }
-              });
-            }
-          }
+              }
 
-        }
+            }
+          } else {
+            pending.unshift(response.request._redirect.redirects[0]['redirectUri']);
+          }
+        })
+        .then(function() {
+          if(that.options.crawl == true) that.crawl();
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
       } else {
-        pending.unshift(response.request._redirect.redirects[0]['redirectUri']);
+        if(that.options.crawl == true) that.crawl();
       }
-    })
-    .then(function() {
-      if(that.options.crawl == true) that.crawl();
-    })
-    .catch(function (error) {
-      console.log(error);
     });
   } else {
     if(that.options.crawl == true) that.crawl();
   }
+
+
+
 };
 
 Teatime.prototype.crawl = function() {
